463856bd7dcd9176bcd577c849f0aa7e6e1e6bf5
Willian <haaswillian@gmail.com>
2017-06-10 23:23:44 -0300

    Removed MainView

--- reducers/errors.js
+++ reducers/errors.js
@@ -3,13 +3,21 @@ import {
 } from '../actions/types';
 
 const initialState = {
-  errors: []
+  errors: [
+    {
+      type: 'error',
+      message: 'Could not access your facebook account. Please, try again.'
+    }
+  ]
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FACEBOOK_LOGIN_FAIL:
-      return [...state.errors, { message: 'Could not access your facebook account. Please, try again.' }];
+      return [...state.errors, {
+        type: 'error',
+        message: 'Could not access your facebook account. Please, try again.'
+      }];
     default:
       return state;
   }
